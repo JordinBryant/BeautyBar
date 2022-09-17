da08c49dacb10302d18e29169b9ea91273a04816
Jordin Bryant <jordintaylor095@gmail.com>
2022-09-17 15:43:58 -0400

    pushing files

--- server.js
+++ server.js
@@ -90,6 +90,6 @@ app.get('/Home/:id', (req, res) => {
     });
 
 });
-app.listen(3000, () => {
-    console.log('listening....');
-});
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
